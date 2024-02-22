e0e399ed6ef79ad9b19e47f2b479ddb13319a1df
halwohamda <halwohamda@gmail.com>
2024-02-21 20:41:23 -0800

    done

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='text-4xl text-red-200'>App</div>
+    <div className='text-4xl text-red-200 text-center'>
+
+   <div>
+    <h2></h2>
+   </div>
+    </div>
   )
 }
 
